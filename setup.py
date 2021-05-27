046c8129c9427c378084644b2fbd6b092ec46d59
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2021-05-27 16:38:09 +0200

    Migrate package to ROS 2 / ament_python
    This is necessary so that the package can be used with the current
    version of the TriFinger robot cluster.

--- setup.py
+++ setup.py
@@ -1,9 +1,51 @@
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup, find_packages
+import os
 
-d = generate_distutils_setup(
-    packages=["env", "mp", "pybullet_planning", "residual_learning", "cic", "cpc"],
+package_name = "rrc"
+
+
+def find_package_data(base_dir, data_dir):
+    """Get list of all files in base_dir/data_dir, relative to base_dir."""
+    paths = []
+    for (path, directories, filenames) in os.walk(
+        os.path.join(base_dir, data_dir)
+    ):
+        for filename in filenames:
+            paths.append(
+                os.path.relpath(os.path.join(path, filename), base_dir)
+            )
+    return paths
+
+
+setup(
+    name=package_name,
+    version="1.1.0",
+    packages=find_packages("python"),
     package_dir={"": "python"},
+    data_files=[
+        (
+            "share/ament_index/resource_index/packages",
+            ["resource/" + package_name],
+        ),
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=False,
+    scripts=[
+        "scripts/run_episode.py",
+        "scripts/run_local_episode.py",
+    ],
+    include_package_data=True,
+    package_data={
+        "residual_learning": [
+            "*.pt",
+            "models/cic_lvl3/logs/*.gin",
+            "models/cic_lvl4/logs/*.gin",
+            "models/cpc_lvl3/logs/*.gin",
+            "models/cpc_lvl4/logs/*.gin",
+            "models/mp_lvl3/logs/*.gin",
+            "models/mp_lvl4/logs/*.gin",
+        ],
+        "cic": ["trifinger_mod.urdf"],
+    },
 )
-
-setup(**d)
