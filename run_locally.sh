265f686f7ba199f374a114e8e0d424defbc40502
Chip Schaff <cbschaff@ttic.edu>
2021-06-03 10:57:54 -0500

    update run_locally.sh for ros 2

--- run_locally.sh
+++ run_locally.sh
@@ -22,12 +22,13 @@ fi
 mkdir -p ${expdir}/catkin_ws/src/usercode
 mkdir -p ${expdir}/logs
 cp -r ${rrc_root}/python ${expdir}/catkin_ws/src/usercode
-cp -r ${rrc_root}/*.txt ${expdir}/catkin_ws/src/usercode
 cp -r ${rrc_root}/*.json ${expdir}/catkin_ws/src/usercode
 cp -r ${rrc_root}/*.xml ${expdir}/catkin_ws/src/usercode
 cp -r ${rrc_root}/setup.py ${expdir}/catkin_ws/src/usercode
+cp -r ${rrc_root}/setup.cfg ${expdir}/catkin_ws/src/usercode
 cp -r ${rrc_root}/scripts ${expdir}/catkin_ws/src/usercode
-singularity exec --cleanenv --contain -B ${expdir}/catkin_ws:/ws ${rrc_image} bash -c ". /setup.bash; cd /ws; catbuild"
+cp -r ${rrc_root}/resource ${expdir}/catkin_ws/src/usercode
+singularity exec --cleanenv --contain -B ${expdir}/catkin_ws:/ws ${rrc_image} bash -c ". /setup.bash; cd /ws; colcon build"
 
 # run command
-singularity exec --cleanenv --contain --nv -B ${expdir}/catkin_ws:/ws,${expdir}/logs:/logdir,/run,/dev ${rrc_image} bash -c ". /setup.bash; . /ws/devel/setup.bash; ${*:2}"
+singularity exec --cleanenv --contain --nv -B ${expdir}/catkin_ws:/ws,${expdir}/logs:/logdir,/run,/dev ${rrc_image} bash -c ". /setup.bash; . /ws/install/setup.bash; ${*:2}"
